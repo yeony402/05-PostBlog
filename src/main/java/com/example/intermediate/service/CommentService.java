c0b0b49e42914e8194f5a03eec67a6ce26165af2
yeony402 <44489399+yeony402@users.noreply.github.com>
2022-09-02 14:57:59 +0900

    Update CommentService.java

--- src/main/java/com/example/intermediate/service/CommentService.java
+++ src/main/java/com/example/intermediate/service/CommentService.java
@@ -27,21 +27,6 @@ public class CommentService {
 
   @Transactional
   public ResponseDto<?> createComment(CommentRequestDto requestDto, HttpServletRequest request) {
-//<<<<<<< HEAD
-//    if (null == request.getHeader("Refresh-Token")) {
-//      return ResponseDto.fail("MEMBER_NOT_FOUND",
-//          "로그인이 필요합니다.");
-//    }
-//
-//    if (null == request.getHeader("Authorization")) {
-//      return ResponseDto.fail("MEMBER_NOT_FOUND",
-//          "로그인이 필요합니다.");
-//    }
-//
-//    Member member = validateMember(request);
-//    if (null == member) {
-//      return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
-//=======
     Member member = validateMember(request);
     if (null == member) {
       return ResponseDto.fail("INVALID_TOKEN", "refresh token is invalid");
@@ -49,9 +34,6 @@ public class CommentService {
 
     Post post = postService.isPresentPost(requestDto.getPostId());
     if (null == post) {
-//<<<<<<< HEAD
-//      return ResponseDto.fail("NOT_FOUND", "존재하지 않는 게시글 id 입니다.");
-//=======
       return ResponseDto.fail("NOT_FOUND", "post id is not exist");
     }
 
@@ -76,9 +58,6 @@ public class CommentService {
   public ResponseDto<?> getAllCommentsByPost(Long postId) {
     Post post = postService.isPresentPost(postId);
     if (null == post) {
-//<<<<<<< HEAD
-//      return ResponseDto.fail("NOT_FOUND", "존재하지 않는 게시글 id 입니다.");
-//=======
       return ResponseDto.fail("NOT_FOUND", "post id is not exist");
     }
 
@@ -95,15 +74,6 @@ public class CommentService {
                       .createdAt(comment.getCreatedAt())
                       .modifiedAt(comment.getModifiedAt())
                       .build()
-//=======
-//          CommentResponseDto.builder()
-//              .id(comment.getId())
-//              .author(comment.getMember().getNickname())
-//              .content(comment.getContent())
-//              .createdAt(comment.getCreatedAt())
-//              .modifiedAt(comment.getModifiedAt())
-//              .build()
-//>>>>>>> 93b3553a138c386df57e8418da661997334c5180
       );
     }
     return ResponseDto.success(commentResponseDtoList);
@@ -111,21 +81,6 @@ public class CommentService {
 
   @Transactional
   public ResponseDto<?> updateComment(Long id, CommentRequestDto requestDto, HttpServletRequest request) {
-//<<<<<<< HEAD
-//    if (null == request.getHeader("Refresh-Token")) {
-//      return ResponseDto.fail("MEMBER_NOT_FOUND",
-//          "로그인이 필요합니다.");
-//    }
-//
-//    if (null == request.getHeader("Authorization")) {
-//      return ResponseDto.fail("MEMBER_NOT_FOUND",
-//          "로그인이 필요합니다.");
-//    }
-//
-//    Member member = validateMember(request);
-//    if (null == member) {
-//      return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
-//=======
     Member member = validateMember(request);
     if (null == member) {
       return ResponseDto.fail("INVALID_TOKEN", "refresh token is invalid");
@@ -133,21 +88,11 @@ public class CommentService {
 
     Post post = postService.isPresentPost(requestDto.getPostId());
     if (null == post) {
-//<<<<<<< HEAD
-//      return ResponseDto.fail("NOT_FOUND", "존재하지 않는 게시글 id 입니다.");
-//=======
       return ResponseDto.fail("NOT_FOUND", "post id is not exist");
     }
 
     Comment comment = isPresentComment(id);
     if (null == comment) {
-//<<<<<<< HEAD
-//      return ResponseDto.fail("NOT_FOUND", "존재하지 않는 댓글 id 입니다.");
-//    }
-//
-//    if (comment.validateMember(member)) {
-//      return ResponseDto.fail("BAD_REQUEST", "작성자만 수정할 수 있습니다.");
-//=======
       return ResponseDto.fail("NOT_FOUND", "comment id is not exist");
     }
 
@@ -169,21 +114,6 @@ public class CommentService {
 
   @Transactional
   public ResponseDto<?> deleteComment(Long id, HttpServletRequest request) {
-//<<<<<<< HEAD
-//    if (null == request.getHeader("Refresh-Token")) {
-//      return ResponseDto.fail("MEMBER_NOT_FOUND",
-//          "로그인이 필요합니다.");
-//    }
-//
-//    if (null == request.getHeader("Authorization")) {
-//      return ResponseDto.fail("MEMBER_NOT_FOUND",
-//          "로그인이 필요합니다.");
-//    }
-//
-//    Member member = validateMember(request);
-//    if (null == member) {
-//      return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
-//=======
     Member member = validateMember(request);
     if (null == member) {
       return ResponseDto.fail("INVALID_TOKEN", "refresh token is invalid");
@@ -191,13 +121,6 @@ public class CommentService {
 
     Comment comment = isPresentComment(id);
     if (null == comment) {
-//<<<<<<< HEAD
-//      return ResponseDto.fail("NOT_FOUND", "존재하지 않는 댓글 id 입니다.");
-//    }
-//
-//    if (comment.validateMember(member)) {
-//      return ResponseDto.fail("BAD_REQUEST", "작성자만 수정할 수 있습니다.");
-//=======
       return ResponseDto.fail("NOT_FOUND", "comment id is not exist");
     }
 
